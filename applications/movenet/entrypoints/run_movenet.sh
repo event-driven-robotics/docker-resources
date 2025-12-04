b65a36408ab759d1b0e3e08617cc493ece09c099
Massimiliano Iacono <massimiliano.iacono@iit.it>
2025-12-04 11:38:32 +0100

    Adding applications folder to run specific tasks

---
+++ applications/movenet/entrypoints/run_movenet.sh
@@ -0,0 +1,4 @@
+yarp detect --write
+echo "Sleeping for 5 seconds"
+sleep 5
+edpr-april --f_vis 30  --f_det 10 --confidence 0.4
---
+++ applications/optical-flow/entrypoints/run_optical_flow.sh
@@ -0,0 +1,4 @@
+yarp detect --write
+echo "Sleeping for 5 seconds"
+sleep 5
+vFramer --flow --src /atis3/AE:o --height 480 --width 640
